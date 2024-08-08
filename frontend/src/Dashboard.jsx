19f3200f2222f39ddee23e6e6df025fb940a110c
Leah Nyambura Mbuthia <leah.mbuthia@griffinglobaltech.com>
2024-08-08 10:00:01 +0300

    Dashboard and Cards

---
+++ frontend/src/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './component/Card'
+
+const Dashboard = () => {
+  return (
+    <div>Dashboard
+        <Card/>
+    </div>
+  )
+}
+
+export default Dashboard
