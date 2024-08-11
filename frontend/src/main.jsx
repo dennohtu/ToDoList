781b088ce61c9e0098af61b00f24af4939e3c519
Stanley Nganga Njoroge <stanley.nnjoroge@griffinglobaltech.com>
2024-08-11 18:08:54 +0300

    i removed strict mode temporaly

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import "./index.css"
+
+// Disable StrictMode temporarily
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
